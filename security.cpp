b209ae0c6e1c779b1445374bac9cdf7168e86ed4
Zaid Hasan <you@example.com>
2023-01-30 20:34:03 +0530

    ok

--- security.cpp
+++ security.cpp
@@ -2,13 +2,14 @@
 #include<fstream>
 #include<sstream>   //to provide templates and type that enable inter operation between string buffer and string types
 #include<string>
+#include<cstring>
 
 using namespace std;
 int main()
 {
 
     int a,i=0,j=0;
-    string text,old,password0,password1,password2,pass,name,age,user,word,word1,cp[100],creds[100];
+    string text,old,password0,password1,password2,pass,name,age,user,word,word1,cp[100],creds[2];
     
     cout<<"         Security System         "<<endl<<endl;
     cout<<"_________________________________"<<endl;
@@ -33,19 +34,20 @@ int main()
                 cin>>password0;
                 cout<<"Enter your age:"<<endl;
                 cin>>age;
-                ofstream of1;   //creating an object of type ofstream class; ofsream helps us to read write and change contents in a particular file
-                of1.open("file.txt");//creating a text file or opening it if it already exists
-                if(of1.is_open())
+                fstream fout; //creating an object of type ofstream class; ofsream helps us to read write and change contents in a particular file
+                fout.open("file.txt", ios::out | ios::app); 
+                //creating a text file or opening it if it already exists
+                if(fout.is_open())
                 {
-                    of1<<name<<"\n";
-                    of1<<password0<<"\n";//readind data in the file
+                    fout<<name<<"\n";
+                    fout<<password0<<"\n";//readind data in the file
                     cout<<"Registration is Successful!"<<endl;
                 }
-                of1.close();
                 break;
             }
             case 2:
             {
+                i=0;
                 cout<<"_________________________________"<<endl;
                 cout<<"|-------------LOGIN-------------|"<<endl;
                 cout<<"|_______________________________|"<<endl<<endl;
@@ -53,7 +55,7 @@ int main()
                 cin>>name;
                 cout<<"Enter your password :-  ";
                 cin>>password0;
-
+                
                 ifstream if1;// ifstream class only helps in reading the cotents of the file
                 if1.open("file.txt");
                 if(if1.is_open())
@@ -66,31 +68,24 @@ int main()
                             iss>>word;//using extraction operator to extract the word from the text file and the store it in credentials array to compare the details
                             creds[i]=word;
                             i++;
+                            cout<<"hi";
                         }
-                    }
-                    int d=1,b=0,c=1;
-                    while(!if1.eof() && i>=c)
-                    {
-                        if(creds[b]==user && pass ==creds[c])
+                        if( name==creds[0] && pass ==creds[1])
                         {
                             cout<<"|-------LOGIN SUCCESSFUL--------|"<<endl<<endl;
                             cout<<"User details are:"<<endl;
                             cout<<"Username is :- "<<user<<endl;
                             cout<<"Password is :- "<<pass<<endl;
-                            d=0;
                             break;
                         }
-                        b+=2;
-                        c+=2;
-                    }
-                    if(d==1)
-                    {
-                        cout<<"         Incorrect credentials"<<endl<<endl;
-                        cout<<"         Enter 2 to Login Again"<<endl;
-                        cout<<"         Enter 3 to Change password"<<endl<<endl;
-                    }
+                        else
+                        {
+                            cout<<"         Incorrect credentials"<<endl<<endl;
+                            cout<<"         Enter 2 to Login Again"<<endl;
+                            cout<<"         Enter 3 to Change password"<<endl<<endl;
+                        }
+                    }                     
                 }
-                if1.close();
                 break;
             }
             case 3:
@@ -159,4 +154,5 @@ int main()
         }
     } while (a>0&&a<4);
     
+    return 1;
 }
